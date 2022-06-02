0d0bff627da22a25f2349711f8d4c541e6edf181
Eric <eciuranam@gmail.com>
2022-06-02 11:34:18 +0200

    006 Constantes

---
+++ 006_Constantes.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract Constantes {
+    // Por convencion, las constantes se nombran siempre con mayusculas
+    uint public constant MY_STATUS = 789;
+}
