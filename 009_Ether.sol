a2a9f57745fd47c42d5bada33bfe222fe351ff7e
Eric <eciuranam@gmail.com>
2022-06-04 19:55:06 +0200

    009 Ether

---
+++ 009_Ether.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+
+
+1 wei  = 1 * 10 ** (-18) ether
+1 kwei = 1 * 10 ** (-15) ether
+1 Mwei = 1 * 10 ** (-12) ether
+1 Gwei = 1 * 10 ** (-9) ether
+1 Twei = 1 * 10 ** (-6) ether
+1 Pwei = 1 * 10 ** (-3) ether
+1 ether = 1 ether
