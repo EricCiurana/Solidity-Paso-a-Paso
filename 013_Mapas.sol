c0907f5e1555544d9b2d7c8b268ba03a581db239
Eric <eciuranam@gmail.com>
2022-06-10 13:08:37 +0200

    013 Mapas

---
+++ 013_Mapas.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+//mapping(keyType => valueType)
+
+contract Mappings {
+	uint public contador;
+	mapping(address => uint) public miMapa;
+
+	function set(uint _num) public {
+		miMapa[msg.sender] = _num;
+		contador++;
+	}
+
+	function get() public view returns (uint) {
+		return miMapa[msg.sender];
+	}
+}
